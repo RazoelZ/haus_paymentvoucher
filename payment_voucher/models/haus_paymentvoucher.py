9d91d336b5c8e77e3af40a2f1255ebfd41904a47
RazoelZ <danydarmawannn@gmail.com>
2023-08-11 14:23:21 +0700

    adding change status

--- payment_voucher/models/haus_paymentvoucher.py
+++ payment_voucher/models/haus_paymentvoucher.py
@@ -12,7 +12,7 @@ class HausPaymentVoucher(models.Model):
     # Field Atas
     no_voucher = fields.Char(String="No")
     date = fields.Date(String="Date", default=datetime.today())
-    due_date = fields.Date(String="Due Date")
+    due_date = fields.Date(String="Due Date", required=True)
     bankacc = fields.Char(String="Bank Account")
     bankacc_holder = fields.Char(String="Bank Account Holder")
     bank_name = fields.Char(String="Bank Name")
@@ -20,6 +20,7 @@ class HausPaymentVoucher(models.Model):
         [('waiting', 'Waiting'),
          ('ASM', 'Appilcation Support Manager'),
          ('FD', 'Financial Dept'),
+         ('done', 'Done'),
          ('notapprove', 'Not Approved')], string="Status", default="waiting")
 
     @ api.constrains('date', 'due_date')
@@ -34,7 +35,6 @@ class HausPaymentVoucher(models.Model):
     deptcode = fields.Char(String="Dept. Code")
     amount = fields.Char(String="Amount")
     amount_alphabet = fields.Char(String="Amount Alphabet")
-
 
     # purpose = fields.Char(String="Purpose")
     requested = fields.Char(String="Requested")
@@ -54,7 +54,7 @@ class HausPaymentVoucher(models.Model):
     good_receipt_notes_no = fields.Char(String="NO. GOOD RECEIPT NOTES")
     tax_invoice_no = fields.Char(String="NO. FAKTUR PAJAK")
     purchase_order_no = fields.Char(String="NO. PURCHASE ORDER")
-    
+
     fields.Selection([
         ('kwitansi', 'NO. KWITANSI'),
         ('invoice', 'NO. INVOICE'),
@@ -65,17 +65,32 @@ class HausPaymentVoucher(models.Model):
     ], string="Receipt")
 
     # Signature Field
-    requested_signature = fields.Binary(string='Requested Signature', widget='sign')
+    requested_signature = fields.Binary(
+        string='Requested Signature', widget='sign')
     requested_name = fields.Char(String="Requested By")
-    approved_signature = fields.Binary(string='Approved Signature', widget='sign')
+    approved_signature = fields.Binary(
+        string='Approved Signature', widget='sign')
     approved_name = fields.Char(String="Approveed By")
     paid_signature = fields.Binary(string='Paid Signature', widget='sign')
     paid_name = fields.Char(String="Paid By")
-    received_signature = fields.Binary(string='Received Signature', widget='sign')
+    received_signature = fields.Binary(
+        string='Received Signature', widget='sign')
     received_name = fields.Char(String="Received By")
 
-    approved_status = fields.Selection(
-    [('waiting', 'Waiting'),
-        ('ASM', 'Appilcation Support Manager'),
-        ('FD', 'Financial Dept'),
-        ('notapprove', 'Not Approved')], string="Status", default="waiting")
+    @api.onchange('requested_signature', 'requested_name')
+    def change_to_apm(self):
+        if self.requested_signature and self.requested_name:
+            self.approved_status = 'ASM'
+
+    @api.onchange('approved_signature', 'approved_name', 'requested_signature', 'requested_name')
+    def change_to_fd(self):
+        if self.approved_signature and self.approved_name and self.requested_signature and self.requested_name:
+            self.approved_status = 'FD'
+
+    @api.onchange('paid_signature', 'paid_name', 'approved_signature', 'approved_name', 'requested_signature', 'requested_name', 'received_signature', 'received_name')
+    def change_to_done(self):
+        if self.paid_signature and self.paid_name and self.approved_signature and self.approved_name and self.requested_signature and self.requested_name and self.received_signature and self.received_name:
+            self.approved_status = 'done'
+
+    def not_approved(self):
+        self.approved_status = 'notapprove'
